a0dbe9f37cbd8cc3a4fad5c809ddcc962a0adf8b
dryman <idryman@gmail.com>
2012-10-23 10:09:42 +0800

    draft on README

--- cwb_crawler.pl
+++ cwb_crawler.pl
@@ -2,3 +2,17 @@
 
 use 5.010;
 use LWP::Simple;
+use JSON;
+use encoding 'utf8';
+use Data::Dumper;
+{
+    package Data::Dumper;
+    sub qquote { return shift; }
+}
+$Data::Dumper::Useperl = 1;
+
+#binmode STDOUT, 'utf8';
+
+my $content = get $ARGV[0];
+my $perl_obj = decode_json $content;
+say Dumper $perl_obj;
